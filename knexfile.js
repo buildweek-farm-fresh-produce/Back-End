50e523bcb8cd9760341120a55f55c68232766e4d
Armando Roman <armandoroman1016@gmail.com>
2019-09-23 20:09:02 -0700

    added pg support

--- knexfile.js
+++ knexfile.js
@@ -34,22 +34,13 @@ module.exports = {
   },
 
   production: {
-    client: 'sqlite3',
-    useNullAsDefault: true,
-    connection: {
-      filename: './data/farm_fresh.db3'
-    },
+    client: 'pg',
+    connection: process.env.DATABASE_URL,
     migrations: {
       directory: './data/migrations'
     },
     seeds: {
       directory: './data/seeds'
-    },
-    pool: {
-      afterCreate: (conn, done) => {
-        conn.run('PRAGMA foreign_keys = ON', done);
-      },
-    },
-  },
-
+    }
+    }
 };
