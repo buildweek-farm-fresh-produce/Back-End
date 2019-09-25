a70a25ad42caf942bfc1f49b74c8f76f3ed3ed8b
Armando Roman <armandoroman1016@gmail.com>
2019-09-24 19:49:44 -0700

    login and register complete on farmer user type, docs updated

---
+++ models/users/farmer_user_model.js
@@ -0,0 +1,31 @@
+const db = require('../../data/dbConfig.js')
+
+module.exports = {
+    add,
+    find,
+    findById,
+    findByUsername,
+}
+
+function add(userData){
+    return db('user_farmer')
+    .insert(userData)
+    .then( ([id]) => findById(id))
+    .catch(err => err);
+}
+
+function find(){
+    return db('user_farmer');
+}
+
+function findById(id){
+    return db('user_farmer')
+    .where({id: id})
+    .first();
+}
+
+function findByUsername(username){
+    return db('user_farmer')
+    .where({username: username})
+    .first();
+}
